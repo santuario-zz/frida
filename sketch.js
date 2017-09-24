63dc1f34499672f056094c949bc7addba0d23e27
adrian santuario <santuario@ciencias.unam.mx>
2017-09-24 05:58:10 -0500

    First testing

--- sketch.js
+++ sketch.js
@@ -2,11 +2,19 @@ var ctracker;
 var videoInput
 
 
+//nose
+var imgNome;
+var imgGoggle;
+
+
+var _isTesting = false;
+
+
 function setup() {
 
   // setup camera capture
   videoInput = createCapture(VIDEO);
-  videoInput.size(640, 480);
+  videoInput.size(480, 480);
   videoInput.position(0, 0);
   videoInput.id('p5video');
 
@@ -23,27 +31,53 @@ function setup() {
   ctracker.start(videoInput.elt);
 
 
+  //images
+  loadImages();
+
   noStroke();
-  
+  background(51);
+}
+
+function loadImages() {
+
+  imgNome = loadImage("assets/images/frida_MASTER_NOSE_0.png"); // Load the image
+  imgGoggle = loadImage("assets/images/frida_MASTER_GOGGLE_0.png"); // Load the image
+
 }
 
 function draw() {
- clear();
+  clear();
 
   // get array of face marker positions [x, y] format
   var positions = ctracker.getCurrentPosition();
- if(positions.length > 0) {
-  for (var i = 0; i < positions.length; i++) {
-    // set the color of the ellipse based on position on screen
-    fill(map(positions[i][0], width * 0.33, width * 0.66, 0, 255), map(positions[i][1], height * 0.33, height * 0.66, 0, 255), 255);
-    // draw ellipse at each position point
+  if (positions.length > 0) {
 
-    ellipse(positions[i][0], positions[i][1], 10, 10);
+    if (_isTesting) {
+
+      for (var i = 0; i < positions.length; i++) {
+        // set the color of the ellipse based on position on screen
+        fill(map(positions[i][0], width * 0.33, width * 0.66, 0, 255), map(positions[i][1], height * 0.33, height * 0.66, 0, 255), 255);
+        // draw ellipse at each position point
+        ellipse(positions[i][0], positions[i][1], 10, 10);
+      }
+    }
+
+
+    var distanceGoggle = abs(positions[0][0] - positions[14][0]);
+    
+    image(imgGoggle, positions[0][0], positions[0][1]-distanceGoggle/3, distanceGoggle, distanceGoggle*0.6);
+
+    var distance = abs(positions[44][0] - positions[50][0]);
+    //print('DISTANCE :: ' + distance);
+    image(imgNome, positions[41][0] - distance, positions[41][1], distance * 2, distance * 2);
+
+
+
   }
- }
-  
 
-  
+
+
+
 
 }
 
