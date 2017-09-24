933c1f4a595581515efbaae43b70ecf367dd79e2
adrian santuario <santuario@ciencias.unam.mx>
2017-09-24 01:54:19 -0500

    Size Canvas

--- sketch.js
+++ sketch.js
@@ -1,40 +1,52 @@
 var ctracker;
-var slider;
+var videoInput
+
 
 function setup() {
 
   // setup camera capture
-  var videoInput = createCapture(VIDEO);
-  videoInput.size(400, 300);
+  videoInput = createCapture(VIDEO);
+  videoInput.size(640, 480);
   videoInput.position(0, 0);
-  
+  videoInput.id('p5video');
+
   // setup canvas
-  var cnv = createCanvas(400, 300);
+  var cnv = createCanvas(windowWidth, windowHeight);
+  cnv.id('p5canvas');
   cnv.position(0, 0);
+
+
 
   // setup tracker
   ctracker = new clm.tracker();
   ctracker.init(pModel);
   ctracker.start(videoInput.elt);
 
-  slider = createSlider(0, 50, 10);
-  slider.position(0, 400);
 
   noStroke();
+  
 }
 
 function draw() {
-  clear();
-  
+ clear();
+
   // get array of face marker positions [x, y] format
   var positions = ctracker.getCurrentPosition();
-  
-  for (var i=0; i<positions.length; i++) {
+ if(positions.length > 0) {
+  for (var i = 0; i < positions.length; i++) {
     // set the color of the ellipse based on position on screen
-    fill(map(positions[i][0], width*0.33, width*0.66, 0, 255), map(positions[i][1], height*0.33, height*0.66, 0, 255), 255);
+    fill(map(positions[i][0], width * 0.33, width * 0.66, 0, 255), map(positions[i][1], height * 0.33, height * 0.66, 0, 255), 255);
     // draw ellipse at each position point
-    var val = slider.value();
-    ellipse(positions[i][0], positions[i][1], val, val);
-  }
 
+    ellipse(positions[i][0], positions[i][1], 10, 10);
+  }
+ }
+  
+
+  
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
