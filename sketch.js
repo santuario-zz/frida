c23d74d0443ee6d483da3d5b451f9b0dab039eb3
adrian santuario <santuario@ciencias.unam.mx>
2017-09-24 22:06:17 -0500

    Rotation Images

--- sketch.js
+++ sketch.js
@@ -2,13 +2,23 @@ var ctracker;
 var videoInput
 
 
-//nose
-var imgNome;
+//frida
+var imgOver_0;
+var imgNose_0;
+var imgNose_1;
 var imgGoggle;
+var imgEarsL;
+var imgEarsR;
+var dogSound;
+var loaderCounter = 0;
 
 
 var _isTesting = false;
 
+/*
+function preload() {
+  dogSound = loadSound('assets/sounds/dog_1.mp3');
+}*/
 
 function setup() {
 
@@ -34,14 +44,29 @@ function setup() {
   //images
   loadImages();
 
+  //sound
+  dogSound = loadSound('assets/sounds/dog.mp3', soundLoaded);
+
+
   noStroke();
   background(51);
 }
 
+function soundLoaded() {
+
+  dogSound.loop();
+  dogSound.setVolume(0.0);
+
+}
+
 function loadImages() {
 
-  imgNome = loadImage("assets/images/frida_MASTER_NOSE_0.png"); // Load the image
+  imgNose_0 = loadImage("assets/images/frida_MASTER_NOSE_0.png"); // Load the image
+  imgNose_1 = loadImage("assets/images/frida_MASTER_NOSE_1.png"); // Load the image
   imgGoggle = loadImage("assets/images/frida_MASTER_GOGGLE_0.png"); // Load the image
+  imgEarsL = loadImage("assets/images/frida_MASTER_EARSL_0.png"); // Load the image
+  imgEarsR = loadImage("assets/images/frida_MASTER_EARSR_0.png"); // Load the image
+  imgOver_0 = loadImage("assets/images/frida_MASTER_OVER_0.png"); // Load the image
 
 }
 
@@ -63,22 +88,46 @@ function draw() {
     }
 
 
-    var distanceGoggle = abs(positions[0][0] - positions[14][0]);
-    
-    image(imgGoggle, positions[0][0], positions[0][1]-distanceGoggle/3, distanceGoggle, distanceGoggle*0.6);
+    var distanceEars = abs(positions[4][0] - positions[10][0]);
+    //image(imgEars, positions[0][0] - distanceEars / 4, positions[21][1] - distanceEars / 5, distanceEars * 1.5, distanceEars * 1.5 * 0.6);
+    image(imgEarsL, positions[0][0] - distanceEars / 2, positions[21][1] - distanceEars / 3, distanceEars, distanceEars * 1.5);
+    image(imgEarsR, positions[14][0] - distanceEars / 2, positions[17][1] - distanceEars / 3, distanceEars, distanceEars * 1.5);
+
+
+
+    var v1Goggle = createVector(positions[0][0], positions[0][1]);
+    var v2Goggle = createVector(positions[14][0], positions[14][1]);
+    var distanceGoggle = abs(v1Goggle.x - v2Goggle.x);
+    var angleGoggle = Math.atan2(v2Goggle.y - v1Goggle.y, v2Goggle.x - v1Goggle.x);
+
+    applyMatrix();
+    translate(v1Goggle.x, v1Goggle.y - distanceGoggle / 3);
+    rotate(angleGoggle);
+    image(imgGoggle, 0, 0, distanceGoggle * 1.1, distanceGoggle * 1.1 * 0.6);
+    resetMatrix();
+    //image(imgGoggle, positions[0][0], positions[0][1] - distanceGoggle / 3, distanceGoggle * 1.1, distanceGoggle * 1.1 * 0.6);
 
     var distance = abs(positions[44][0] - positions[50][0]);
-    //print('DISTANCE :: ' + distance);
-    image(imgNome, positions[41][0] - distance, positions[41][1], distance * 2, distance * 2);
+    var mouthH = positions[57][1] - positions[60][1];
+    var faceH = positions[7][1] - positions[33][1];
+    var mouthR = mouthH / faceH;
 
+    applyMatrix();
+    translate(positions[41][0] - distance, positions[41][1]);
+    rotate(angleGoggle);
+    if (mouthR > 0.1) {
+      image(imgNose_0, 0, 0, distance * 2, distance * 2);
+      dogSound.setVolume(1.0);
+    } else {
+      image(imgNose_1, 0, 0, distance * 2, distance * 2);
+      dogSound.setVolume(0.0);
+    }
+    resetMatrix();
+
+   //image(imgOver_0, 0, 0, width, width);
 
 
   }
-
-
-
-
-
 }
 
 function windowResized() {
